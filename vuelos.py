ce074096bb2aaaff40b22c861e8d511ecd7d881d
pmolinag <pablomolinagomez@hotmail.com>
2017-09-28 13:04:02 +0200

    creacion biblioteca

---
+++ vuelos.py
@@ -0,0 +1,7 @@
+class Vuelos:
+    def __init__(self, precio, origen, destino):
+        self.precio = precio
+        self.origen = origen
+        self.destino = destino
+
+## Crear array de precios, origenes y destinos
