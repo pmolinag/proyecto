33976a095e47c4b44cb459d129639fc68ab4ce0b
pmolinag <pablomolinagomez@hotmail.com>
2017-10-02 15:41:08 +0200

    Cambiando formato de markdown

--- vuelos.py
+++ vuelos.py
@@ -1,7 +1,10 @@
+from array import array
+
 class Vuelos:
-    def __init__(self, precio, origen, destino):
-        self.precio = precio
+    def __init__(self, origen, destino):
         self.origen = origen
         self.destino = destino
 
 ## Crear array de precios, origenes y destinos
+
+precios = []
